f9acac27695451997df3645871f54dcc50c07358
shilpamd1521 <87756456+shilpamd1521@users.noreply.github.com>
2021-07-22 01:24:08 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,23 @@
+
+
+public class Hello {
+
+	public static void main(String args[]) {
+	
+		int a[] = {2,3,4,4,7,8,9};
+		
+		int len=a.length;
+		
+		for (int i=0; i< len /2 ; i++ )
+		{
+			int temp = a[len-1-i];
+			a[len-1-i] = a[i];
+			a[i]=temp;
+		}
+		for (int j=0; j<len ;j++)
+		System.out.println(a[j]);
+	
+		
+		System.out.println(15%2);
+	}
+}
